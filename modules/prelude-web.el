fd83b8087e46460728da17987fac4cd02d2f94f0
Jory Graham <jory.graham@gmail.com>
2013-11-27 21:11:15 -0500

    Pushing my newest .emacs.d changes up

--- modules/prelude-web.el
+++ modules/prelude-web.el
@@ -66,7 +66,16 @@
 
 (eval-after-load 'web-mode
   '(progn
-     (defun prelude-web-mode-defaults ())
+     (defun prelude-web-mode-defaults ()
+       ;; Disable whitespace-mode when using web-mode
+       (whitespace-mode -1)
+       ;; Customizations
+       (setq web-mode-markup-indent-offset 2)
+       (setq web-mode-css-indent-offset 2)
+       (setq web-mode-code-indent-offset 2)
+       (setq web-mode-disable-autocompletion t)
+       (local-set-key (kbd "RET") 'newline-and-indent))
+
      (setq prelude-web-mode-hook 'prelude-web-mode-defaults)
 
      (add-hook 'web-mode-hook (lambda ()
